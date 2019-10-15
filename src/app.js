c44227775ee8acd319c85d7750fcd539e24c2f96
Maurício Baú <mauricio.a.bau@gmail.com>
2019-10-15 11:15:50 -0300

    Iniciando sistema

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const rotes = require('./routes');
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+module.exports = new App().server;
