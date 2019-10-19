1e13ebc4673e5006c54f7d4e94167af67208b151
Maurício A. Baú <mauricio.a.bau@gmail.com>
2019-10-19 15:43:42 -0300

    adicionado e  onfigurando templates de e-mail

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,6 +1,11 @@
 // importar nodemailer
 import nodemailer from 'nodemailer';
-
+// importar o path para carregar template de email
+import { resolve } from 'path';
+// importar exphbs
+import exphbs from 'express-handlebars';
+// importar nodemailerhbs
+import nodemailerhbs from 'nodemailer-express-handlebars';
 // importar configurações para envio de email
 import mailConfig from '../config/mail';
 
@@ -13,6 +18,26 @@ class Mail {
       secure,
       auth: auth.user ? auth : null,
     });
+
+    this.configureTemplates();
+  }
+
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
   }
 
   sendMail(message) {
